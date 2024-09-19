aa6c2f0be9adcb0314f45fb7c563603720da4cf1
Павел Поворотный <padrinoexperto@mail.ru>
2024-09-19 19:10:53 +0700

    1

--- mymath.go
+++ mymath.go
@@ -1,4 +1,3 @@
-// mymath.go
 package mymath
 
 import "math"
@@ -7,52 +6,24 @@ func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
 
-// Pow - функция для вычисления возведения числа в степень
-func Pow(x, y float64) float64 {
-	return math.Pow(x, y)
-}
-
-// Sin - функция для вычисления синуса угла
-func Sin(x float64) float64 {
-	return math.Sin(x)
-}
-
-// Cos - функция для вычисления косинуса угла
-func Cos(x float64) float64 {
-	return math.Cos(x)
-}
-
-// Tan - функция для вычисления тангенса угла
-func Tan(x float64) float64 {
-	return math.Tan(x)
-}
-
-// Log - функция для вычисления натурального логарифма
-func Log(x float64) float64 {
-	return math.Log(x)
-}
-
-// Exp - функция для вычисления экспоненты
-func Exp(x float64) float64 {
-	return math.Exp(x)
-}
-
-// Abs - функция для вычисления абсолютного значения числа
 func Abs(x float64) float64 {
 	return math.Abs(x)
 }
 
-// Acos - функция для вычисления арккосинуса числа
+func Max(a float64, b float64) float64 {
+	return math.Max(a, b)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
+
 func Acos(x float64) float64 {
 	return math.Acos(x)
 }
-
-// Acosh - функция для вычисления гиперболического арккосинуса числа
 func Acosh(x float64) float64 {
 	return math.Acosh(x)
 }
-
-// Asin - функция для вычисления арксинуса числа
 func Asin(x float64) float64 {
 	return math.Asin(x)
 }
